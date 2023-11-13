5c3685bc83ce33b36721839708e6dc0dffe51aae
ifyour <ifyour@outlook.com>
2023-11-14 07:36:17 +0800

    fix: error handling

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app
   .get('/', (c) => c.redirect('/translate'))
   .get('/translate', (c) => c.text('Please use POST method :)'))
   .post('/translate', async c => {
-    const params = await c.req.json()
+    const params = await c.req.json().catch(() => ({}))
     const result = await query(params, { proxyEndpoint: 'https://ideepl.vercel.app/jsonrpc' })
     return c.json(result, result.code)
   })
