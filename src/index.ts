8b80e4c7aff838885c0e99062306211b6355b615
ifyour <ifyour@outlook.com>
2023-11-13 23:53:07 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono'
+import { query } from '@ifyour/deeplx'
+
+const app = new Hono()
+
+app
+  .get('/', (c) => c.redirect('/translate'))
+  .get('/translate', (c) => c.text('Please use POST method :)'))
+  .post('/translate', async c => {
+    const params = await c.req.json()
+    const result = await query(params, { proxyEndpoint: 'https://ideepl.vercel.app/jsonrpc' })
+    return c.json(result, result.code)
+  })
+
+export default app
