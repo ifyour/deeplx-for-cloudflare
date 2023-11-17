cd2a6891e13465b19ad330daa4c929d31b6f6fac
ifyour <ifyour@outlook.com>
2023-11-17 23:46:12 +0800

    fix: code style

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Hono()
 app
   .get('/', (c) => c.redirect('/translate'))
   .get('/translate', (c) => c.text('Please use POST method :)'))
-  .post('/translate', async c => {
+  .post('/translate', async (c) => {
     const params = await c.req.json().catch(() => ({}))
     const result = await query(params, { proxyEndpoint: 'https://ideepl.vercel.app/jsonrpc' })
     return c.json(result, result.code)
