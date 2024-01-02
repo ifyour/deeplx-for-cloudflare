1e08a814514e25983395300e23d28e4e12dc502e
ifyour <ifyour@outlook.com>
2024-01-02 21:17:01 +0800

    fix: update proxy endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { Hono } from 'hono'
-import { query } from '@ifyour/deeplx'
+import { Hono } from "hono";
+import { query } from "@ifyour/deeplx";
 
-const app = new Hono()
-
-// Proxy server to resolve error 525, see: https://github.com/cloudflare/workerd/issues/776
-const proxyEndpoint = 'https://cors.mingming.dev/https://www2.deepl.com/jsonrpc'
+const app = new Hono();
 
 app
-  .get('/', (c) => c.redirect('/translate'))
-  .get('/translate', (c) => c.text('Please use POST method :)'))
-  .post('/translate', async (c) => {
-    const params = await c.req.json().catch(() => ({}))
-    const result = await query(params, { proxyEndpoint })
-    return c.json(result, result.code)
-  })
+  .get("/", (c) => c.redirect("/translate"))
+  .get("/translate", (c) => c.text("Please use POST method :)"))
+  .post("/translate", async (c) => {
+    const params = await c.req.json().catch(() => ({}));
+    const result = await query(params, {
+      proxyEndpoint: "https://ideepl.vercel.app/jsonrpc",
+    });
+    return c.json(result, result.code);
+  });
 
-export default app
+export default app;
